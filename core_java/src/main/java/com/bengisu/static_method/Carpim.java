828ca3b941da68c3081c0e879378bf5c37386abc
Bengisu <bengisu.161@gmail.com>
2025-05-30 18:21:31 +0300

    JA-8

---
+++ core_java/src/main/java/com/bengisu/static_method/Carpim.java
@@ -0,0 +1,12 @@
+package com.bengisu.static_method;
+
+public class Carpim
+{
+    public static void carp()
+    {
+        int a=5;
+        int b=7;
+        int carp=a*b;
+        System.out.println("a*b işleminin sonucu: "+carp);
+    }
+}
---
+++ core_java/src/main/java/com/bengisu/static_method/Main.java
@@ -0,0 +1,9 @@
+package com.bengisu.static_method;
+
+public class Main
+{
+    public static void main(String[]args)
+    {
+        Carpim.carp();
+    }
+}
